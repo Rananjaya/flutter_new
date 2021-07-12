6e3d2151d4c09c0126d10a3e2b362dd7455fda4a
Rananjaya Bandara <rananjayabandara@gmail.com>
2021-07-13 00:53:12 +0530

    add asset image

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,8 @@ void main() {
         ),
           body: Center(
           child: Image(
-            image: NetworkImage('https://i.ibb.co/4tJnqZb/ABT41-E4-AD9183134-C8-C2-B18-DCCCE2997-ECF5-AF0-AE6885-C8-B50-B942-DFD8-E47-BDB1-B7.jpg'),
+            // image: NetworkImage('https://i.ibb.co/4tJnqZb/ABT41-E4-AD9183134-C8-C2-B18-DCCCE2997-ECF5-AF0-AE6885-C8-B50-B942-DFD8-E47-BDB1-B7.jpg'),
+               image: AssetImage('images/diamond.png'),
           ),
         )
       ) ,
