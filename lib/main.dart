b01cb33b3cf328347eb8adeaf79438ce9665158b
Rananjaya Bandara <rananjayabandara@gmail.com>
2021-07-13 21:45:57 +0530

    Assingment

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,50 @@ class MyApp  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            child : Text("AAA"),
-          ),
-        ),
-      ),
+         home: Scaffold(
+          backgroundColor: Colors.teal,
+           body: SafeArea(
+                child:Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Text("1 st"),
+                      width: 100.0,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      // width: 100.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100.0,
+                          height: 100.0,
+                          color: Colors.yellow,
+                        ),
+                        Container(
+                          width: 100.0,
+                          height: 100.0,
+                          color: Colors.yellowAccent,
+                        ),
+                      ],
+                    ),
+                    ),
+
+                    Container(
+                      child: Text("2 st"),
+                      width: 100.0,
+                      color: Colors.blue,
+                    ),
+                  ],
+                ) ,
+           ),
+           // appBar: AppBar(
+           //   title: Text('aaa'),
+           //   backgroundColor: Colors.red,
+           // ),
+         ),
     );
   }
 }
